2123f75a10c61f3e3b6c9c258f6c71483deecb8e
Moi <profesormoi@gmail.com>
2020-02-01 11:59:35 +0100

    Nueva versión
    He añadido funcionalidad.
    
    Signed-off-by: Moi <profesormoi@gmail.com>

--- miproyecto/src/miproyecto/Programa.java
+++ miproyecto/src/miproyecto/Programa.java
@@ -5,6 +5,8 @@ public class Programa {
 	public static void main(String[] args) {
 		// TODO Apéndice de método generado automáticamente
 		System.out.println("Hola");
+		System.out.println("Mundo");
+		System.out.println("Qué tal");
 
 	}
 
